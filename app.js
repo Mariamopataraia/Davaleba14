ef32704f97b6eaa49a2dd0068eb3a9dbc27e96bb
Mariamopataraia <mariamopataraia@gmail.com>
2021-10-24 19:37:58 +0400

    added 3 tasks app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// Array sum #!
+const Num = [1, 2, 3, 4, 5]
+let sum = 0
+for (let i = 0; i< Num.length; i++) {
+    sum += Num[i];
+}
+console.log(sum)
+
+// Array Object #2
+const myObj = {
+    name: 'Mathilde',
+    age: '29',
+    address: 'Tbilisi'
+}
+
+// სტრინგების გამოტანა console.log-ში #3
+const myObject = {
+    name: 'Mathilde',
+    age: '29',
+    address: 'Tbilisi'
+}
+console.log("My name is " + myObject.name)
+console.log("My age is " + myObject.age)
+console.log("My address is " + myObject.address)
